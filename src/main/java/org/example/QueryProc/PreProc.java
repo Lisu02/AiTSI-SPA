e2637e16719a4a5ce4c12b1685327b6cc5b820ec
LukaszPB <lukasz.kiersnowski.110752@student.pb.edu.pl>
2025-03-16 23:27:47 +0100

    Added query parsing and validation. The code works, but needs further testing, optimization, and readability improvements

--- src/main/java/org/example/QueryProc/PreProc.java
+++ src/main/java/org/example/QueryProc/PreProc.java
@@ -5,7 +5,7 @@ import org.example.Exceptions.InvalidQueryException;
 import java.util.*;
 
 public class PreProc {
-    private final List<String> entities = List.of(
+    private final Set<String> entities = Set.of(
             "procedure", "stmtLst", "stmt", "assign", "if", "while"
     );
     private final Map<String, RelationSignature> relations = Map.of(
@@ -18,19 +18,68 @@ public class PreProc {
             "Modifies", new RelationSignature(Set.of("procedure", "prog_line", "stmt", "assign", "string", "integer"),
                     Set.of("variable", "string", "_"))
     );
-
-
+    private static final Map<String, Set<String>> attributes = Map.of(
+            "procName", Set.of("procedure", "call"),
+            "varName", Set.of("variable"),
+            "value", Set.of("constant"),
+            "stmt#", Set.of("stmt", "assign", "while", "if", "call")
+    );
     public QueryTree parseQuery(String query) throws InvalidQueryException {
         List<String> queryElements = List.of(query.split(";"));
 
         List<String> queryArguments = queryElements.subList(0,queryElements.size()-1);
         String queryBody = queryElements.get(queryElements.size()-1);
 
+        Map<String,String> synonyms = separateSynonyms(queryArguments);
+        System.out.println(synonyms);
+
+//        System.out.println(queryArguments);
+        String[] bodyElements = Arrays.stream(queryBody.split("(?=\\b(such that|with)\\b)")).filter(e->!e.isBlank()).toArray(String[]::new);
+        System.out.println(Arrays.toString(bodyElements));
+
+        String[] returnValues = Arrays.stream(bodyElements[0]
+                        .split("[ ,]"))
+                .filter(s->!s.isBlank())
+                .toArray(String[]::new);
+
+//        System.out.println(Arrays.toString(returnValues));
+        if(!returnValues[0].equals("Select") && !returnValues[0].equals("Boolean")) {
+            throw new InvalidQueryException("Missing Select: " + query);
+        }
+        for (int i=1;i<returnValues.length;i++) {
+            if(!synonyms.containsKey(returnValues[i])) {
+                throw new InvalidQueryException("Not recognize synonym: " + returnValues[i]);
+            }
+        }
+
+        List<String[]> suchThatStatements = new ArrayList<>();
+        List<String[]> withStatements = new ArrayList<>();
+        for(int i=1;i<bodyElements.length;i++) {
+            if(bodyElements[i].startsWith("such that")) {
+                bodyElements[i] = bodyElements[i].substring(10).trim();
+                String[] tmp = Arrays.stream(bodyElements[i].split("[ ,()]")).filter(t->!t.isBlank()).toArray(String[]::new);
+                suchThatStatements.add(tmp);
+            }
+            else if(bodyElements[i].startsWith("with")) {
+                bodyElements[i] = bodyElements[i].substring(5).trim();
+                String[] tmp = Arrays.stream(bodyElements[i].split("[ .=]")).filter(t->!t.isBlank()).toArray(String[]::new);
+                withStatements.add(tmp);
+            }
+        }
+//        System.out.println("such that:" + suchThatStatements);
+//        System.out.println("with:" + Arrays.toString(withStatements.get(0)));
+
+        validateSuchThatStatements(suchThatStatements,synonyms);
+        validateWithStatements(withStatements,synonyms);
+
+        return crateQueryTree(query);
+    }
+    private Map<String,String> separateSynonyms(List<String> queryArguments) throws InvalidQueryException {
         //creating synonym map (separate them, so they can be easily used)
         Map<String,String> synonyms = new HashMap<>();
         for(String element : queryArguments) {
             String[] tmp = Arrays.stream(element
-                    .split("[ ,]"))
+                            .split("[ ,]"))
                     .filter(s->!s.isBlank())
                     .toArray(String[]::new);
 
@@ -45,27 +94,53 @@ public class PreProc {
                 synonyms.put(tmp[i],tmp[0]);
             }
         }
-
-//        How we will validate relations arguments
-//        RelationSignature rs = relations.get("Modifies");
-//        if(!rs.isArg1TypeAllowed(synonyms.get("a"))) {
-//            throw new InvalidQueryException("Invalid argument: " + synonyms.get("a"));
-//        }
-//        else {
-//            System.out.println("valid argument");
-//        }
-
-        System.out.println(synonyms);
-//        System.out.println(queryArguments);
-//        System.out.println(queryBody);
-
-        if(!validate(query)) {
-            throw new InvalidQueryException("Invalid query: " + query);
-        }
-        return crateQueryTree(query);
+        return synonyms;
     }
-    private boolean validate(String query) {
-        return false;
+    private void validateSuchThatStatements(List<String[]> suchThatStatements, Map<String,String> synonyms) throws InvalidQueryException {
+        for(String[] relation : suchThatStatements) {
+            RelationSignature signature = relations.get(relation[0]);
+            if(signature == null) {
+                throw new InvalidQueryException("Incorrect relation name: " + relation[0]);
+            }
+            String[] type =  new String[2];
+            for(int i=1;i<relation.length;i++) {
+                if(relation[i].matches("\\d+")) {
+                    type[i-1] = "integer";
+                }
+                else if(relation[i].charAt(0) == '"' && relation[i].charAt(relation[i].length()-1) == '"') {
+                    type[i-1] = "string";
+                }
+                else {
+                    type[i-1] = synonyms.get(relation[i]);
+                }
+            }
+            if(signature.isArg1TypeAllowed(type[0])) {
+                throw new InvalidQueryException("Incorrect argument type: " + relation[1] + " - " + type[0] + " in relation " + relation[0]);
+            }
+            else if(signature.isArg2TypeAllowed(type[1])) {
+                throw new InvalidQueryException("Incorrect argument type: " + relation[2] + " - " + type[1] + " in relation " + relation[0]);
+            }
+            System.out.println(signature);
+        }
+    }
+    private void validateWithStatements(List<String[]> withStatements, Map<String,String> synonyms) throws InvalidQueryException {
+        for(String[] statement : withStatements) {
+            String type = synonyms.get(statement[0]);
+            if(type == null) {
+                throw new InvalidQueryException("There is no such synonym as : " + statement[0]);
+            }
+            if(!attributes.get(statement[1]).contains(type)) {
+                throw new InvalidQueryException("Synonym " + statement[0] + " does not have attribute " + statement[1]);
+            }
+            if((statement[1].equals("procName") || statement[1].equals("valName")) &&
+                    (statement[2].charAt(0) != '"' || statement[2].charAt(statement[2].length()-1) != '"')) {
+
+                throw new InvalidQueryException("Incorrect type :" + statement[2] + ". Type should be string");
+            }
+            else if(!statement[2].matches("\\d+")) {
+                throw new InvalidQueryException("Incorrect type :" + statement[2] + ". Type should be integer");
+            }
+        }
     }
     private QueryTree crateQueryTree(String query) {
         return new QueryTree();
--- src/main/java/org/example/QueryProc/QueryController.java
+++ src/main/java/org/example/QueryProc/QueryController.java
@@ -8,12 +8,12 @@ public class QueryController {
     public static void main(String[] args) {
         QueryTree queryTree = null;
         try {
-            queryTree = preProc.parseQuery("stmt s1,s2; assign a; Select s1 such that Follows (s1, 2)");
+            queryTree = preProc.parseQuery("stmt s1,s2; assign a; Select s1, s2 such that Modifies (s1, 2) such that Calls (s1, \"Second\") with a.procName= 19");
         } catch (InvalidQueryException e) {
             System.out.println(e.getMessage());
         }
-        String result = evaluator.evaluateQuery(queryTree);
-        System.out.println(result);
+//        String result = evaluator.evaluateQuery(queryTree);
+//        System.out.println(result);
     }
 
 }
