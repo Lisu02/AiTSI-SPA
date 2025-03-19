b80316a255f85483e3aef38b78e2147f42a885ed
LukaszPB <lukasz.kiersnowski.110752@student.pb.edu.pl>
2025-03-19 09:35:53 +0100

    added test case delivery from file

--- src/test/java/QueryProc/PreProcTest.java
+++ src/test/java/QueryProc/PreProcTest.java
@@ -2,7 +2,14 @@ package QueryProc;
 
 import org.example.Exceptions.InvalidQueryException;
 import org.example.QueryProc.PreProc;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -10,13 +17,18 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class PreProcTest {
     private final PreProc preProc = new PreProc();
 
-    @Test
-    void shouldThrowExceptionWhenRelationNameIsIncorrect() {
-        String query = "stmt s1,s2; procedure p; Select p, s2 such that Modifies (s2, _) such that Cals (p, \"Second\") with p.procName = \"Third\"";
+    static Stream<Arguments> queryProvider() throws IOException {
+        return Files.lines(Path.of("src/test/resources/invalid_query_list"))
+                .map(line -> line.split("#", 2))
+                .map(parts -> Arguments.of(parts[0].trim(), parts[1].trim()));
+    }
+
+    @ParameterizedTest
+    @MethodSource("queryProvider")
+    void shouldThrowExceptionWhenQueryIsInvalid(String query, String expectedMessage) {
         Exception exception = assertThrows(InvalidQueryException.class, () -> {
             preProc.parseQuery(query);
         });
-        assertEquals("Incorrect relation name: Cals",exception.getMessage());
-
+        assertEquals(expectedMessage,exception.getMessage());
     }
 }
