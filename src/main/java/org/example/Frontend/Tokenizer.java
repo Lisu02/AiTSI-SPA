8c54991bd04b96d66634f5c52a9db61c8fd140aa
Lisu02 <terminatorektt8000@gmail.com>
2025-03-21 11:58:37 +0100

    Tokenizer start

--- src/main/java/org/example/Frontend/Tokenizer.java
+++ src/main/java/org/example/Frontend/Tokenizer.java
@@ -3,6 +3,7 @@ package org.example.Frontend;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 import java.util.logging.Logger;
@@ -11,19 +12,37 @@ public class Tokenizer {
 
     Logger log = Logger.getLogger(Tokenizer.class.getName());
 
-    public List<String> getTokensFromFilename(String filename) throws FileNotFoundException {
-        Scanner fileScanner = getScanner(filename);
+
+    // Tokenizer a+b powinien wyciągnąć tokeny a , + , b;
+    public List<String> getTokensFromFilename(String filename){
+        Scanner fileScanner;
+        try{
+            fileScanner = getScanner(filename);
+        } catch (FileNotFoundException e) {
+            log.throwing(Tokenizer.class.getName(), "getTokensFromFilename", e);
+            throw new RuntimeException(e);
+        }
 
         String word;
-        int statmentCounter = 1;
+        int statmentNumber = 1;
         ArrayList<String> tokenArrayList = new ArrayList<>();
 
         while(fileScanner.hasNext()){
             word = fileScanner.next();
             tokenArrayList.add(word);
-            log.fine("Line: "+statmentCounter + " -> " + word);
+            log.info("Line: "+statmentNumber + " -> " + word);
+            if(isNewStatment(word)){statmentNumber++;}
+
+            Iterator<String> iterator = tokenArrayList.iterator();
+            while(iterator.hasNext()){
+                iterator.next();
+            }
         }
         return tokenArrayList;
+    }
+
+    private boolean isNewStatment(String word){
+        return word.endsWith("{") || word.endsWith(";");
     }
 
     public Scanner getScanner(String filename) throws FileNotFoundException {
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,12 @@
 package org.example;
 
+import org.example.Frontend.Tokenizer;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("MAIN EMPTY");
+        Tokenizer tokenizer = new Tokenizer();
+        tokenizer.getTokensFromFilename("SimpleFirst.txt");
     }
 }
