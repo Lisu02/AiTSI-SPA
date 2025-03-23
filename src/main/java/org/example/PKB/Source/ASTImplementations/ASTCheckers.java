1eec3f8ea8afbde2f8bc9379a561bdccb6cfff17
lewy <huak@wp.pl>
2025-03-23 15:30:24 +0100

    Klasa checkers zrobiona

--- src/main/java/org/example/PKB/Source/ASTImplementations/ASTCheckers.java
+++ src/main/java/org/example/PKB/Source/ASTImplementations/ASTCheckers.java
@@ -1,22 +1,99 @@
 package org.example.PKB.Source.ASTImplementations;
 
+import org.example.PKB.API.EntityType;
 import org.example.PKB.API.TNode;
+import org.example.PKB.Source.ASTNode;
 
 public abstract class ASTCheckers extends ASTFollows {
+
+    // Check if n2 follows n1
     public boolean isFollowed(TNode n1, TNode n2)
     {
-        return false; // To be implemented...
+        ASTNode astNode1 = (ASTNode) n1;
+        ASTNode astNode2 = (ASTNode) n2;
+        if(astNode1.getParent()!=null && astNode2.getParent()!=null)
+        {
+            if(astNode1.getParent()==astNode2.getParent())
+            {
+                if(astNode1.getId()==astNode2.getId()+1)
+                {
+                    return true;
+                }
+
+            }
+        }
+        return false;
     }
+    // Check if n2 follows* n1
     public boolean isFollowedAstra(TNode n1, TNode n2)
     {
-        return false; // To be implemented...
+        ASTNode astNode1 = (ASTNode) n1;
+        ASTNode astNode2 = (ASTNode) n2;
+        if(astNode1.getParent()!=null && astNode2.getParent()!=null)
+        {
+            if(astNode1.getParent()==astNode2.getParent())
+            {
+                if(astNode1.getId()>astNode2.getId())
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
+    // Check if n1 is parent n2
     public boolean isParent(TNode n1, TNode n2)
     {
-        return false; // To be implemented...
+        ASTNode astNode1 = (ASTNode) n1;
+        ASTNode astNode2 = (ASTNode) n2;
+        ASTNode parent = astNode2.getParent();
+
+        if(parent!=null)
+        {
+            if(checkIfEntityIsWhileOrIf(astNode1))
+            {
+                if(astNode1==parent)
+                {
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
+    //check if n1 is parent* n2
     public boolean isParentAstra(TNode n1, TNode n2)
     {
-        return false; // To be implemented...
+        ASTNode astNode1 = (ASTNode) n1;
+        ASTNode astNode2 = (ASTNode) n2;
+
+        ASTNode parent = astNode2.getParent();
+        if(checkIfEntityIsWhileOrIf(astNode1))
+        {
+            while (parent != null) {
+                if (parent.getId() < astNode2.getId()) {
+                    break;
+                }
+                if (checkIfEntityIsWhileOrIf(parent)) {
+                    if (parent == astNode1) {
+                        return true;
+                    }
+                }
+                parent = parent.getParent();
+            }
+        }
+
+
+        return false;
+    }
+
+    private boolean checkIfEntityIsWhileOrIf(ASTNode n1)
+    {
+        if(n1.getEntityType()==EntityType.WHILE || n1.getEntityType()==EntityType.IF)
+        {
+            return true;
+        }
+        return false;
     }
 }
