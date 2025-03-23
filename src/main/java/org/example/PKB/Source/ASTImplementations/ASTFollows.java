30dd695ced2efa1959e66dd6d63efc5ed5e56960
lewy <huak@wp.pl>
2025-03-23 14:20:30 +0100

    follows zrobiony

--- src/main/java/org/example/PKB/Source/ASTImplementations/ASTFollows.java
+++ src/main/java/org/example/PKB/Source/ASTImplementations/ASTFollows.java
@@ -1,24 +1,61 @@
 package org.example.PKB.Source.ASTImplementations;
 
 import org.example.PKB.API.TNode;
+import org.example.PKB.Source.ASTNode;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public abstract class ASTFollows extends ASTParents {
-    public TNode getFollows(TNode n)
-    {
-        return null; // To be implemented...
+    public TNode getFollows(TNode n) {
+        try {
+            ASTNode astNode = (ASTNode) n;
+            ASTNode parent = astNode.getParent();
+
+            if (parent != null) {
+                return  parent.getChild(astNode.getId() + 1);
+            }
+        } catch (NullPointerException | IndexOutOfBoundsException e) {
+
+        }
+        return null;
     }
+
     public List<TNode> getFollowsAstra(TNode n)
     {
-        return null; // To be implemented...
+        ASTNode astNode = (ASTNode) n;
+        ASTNode parent = astNode.getParent();
+        if (parent != null) {
+            return parent.getChildren().stream()
+                    .filter(child -> child.getId() > astNode.getId())
+                    .collect(Collectors.toList());
+        }
+        return Collections.emptyList();
     }
     public TNode getFollowedBy(TNode n)
     {
-        return null; // To be implemented...
+        try {
+            ASTNode astNode = (ASTNode) n;
+            ASTNode parent = astNode.getParent();
+
+            if (parent != null) {
+                return  parent.getChild(astNode.getId() - 1);
+            }
+        } catch (NullPointerException | IndexOutOfBoundsException e) {
+
+        }
+       return null;
     }
     public List<TNode> getFollowedAstraBy(TNode n)
     {
-        return null; // To be implemented...
+        ASTNode astNode = (ASTNode) n;
+        ASTNode parent = astNode.getParent();
+        if (parent != null) {
+            return parent.getChildren().stream()
+                    .filter(child -> child.getId() < astNode.getId())
+                    .collect(Collectors.toList());
+        }
+        return Collections.emptyList();
     }
 }
