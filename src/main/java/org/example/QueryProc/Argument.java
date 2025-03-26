0ee8a3e58488f0a2f557f828a62e2ef10d078d49
LukaszPB <lukasz.kiersnowski.110752@student.pb.edu.pl>
2025-03-26 22:09:22 +0100

    Changed a way of storing suchThatStatements and returnValues(probably not a final version). Most importantly, added all relationsDefinition required for first iteration.

---
+++ src/main/java/org/example/QueryProc/Argument.java
@@ -0,0 +1,13 @@
+package org.example.QueryProc;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class Argument {
+    private String name;
+    private String type;
+}
