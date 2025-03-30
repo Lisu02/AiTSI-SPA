9f52aa143a2537a96e702a0c31d6ef7482613752
Lisu02 <terminatorektt8000@gmail.com>
2025-03-30 21:51:23 +0200

    AbstractionExtractor - praca nad IModifies oraz testami z mockito + mvn mock junit [w trakcie pracy]

--- src/main/java/org/example/Frontend/AbstractionExtractor.java
+++ src/main/java/org/example/Frontend/AbstractionExtractor.java
@@ -1,5 +1,85 @@
-package org.example.Frontend;
+    package org.example.Frontend;
 
-public class AbstractionExtractor {
+    import org.example.PKB.API.*;
+
+    import java.util.ArrayList;
+    import java.util.List;
+    import java.util.logging.Logger;
+
+    public class AbstractionExtractor {
+
+        private static final Logger log = Logger.getLogger(AbstractionExtractor.class.getName());
+
+        //Na razie testowo imodifies
+        private IAST iast;
+        private IModifies iModifies;
+
+        public AbstractionExtractor(){
+            this(new IASTImplementationFrontend(),new ImodifiesFrontendImpl());
+        }
+        public AbstractionExtractor(IAST iast){
+            this(iast,new ImodifiesFrontendImpl());
+        }
+        public AbstractionExtractor(IAST iast, IModifies iModifies){
+            this.iast = iast;
+            this.iModifies = iModifies;
+        }
+
+    //Root - program
+    //Procedure
+    //StmtList
+    //Stmt
+    //Stmt
+
+    //Generowanie Modifies ora Uses do PKB
+    public void generateStarterAbstractions(){
+        TNode tNodeRoot = iast.getRoot();
+        List<TNode> procedureList = new ArrayList<>();
+
+        //Listing procedures
+        TNode firstProcedureNode = iast.getLinkedNode(LinkType.FirstChild,tNodeRoot);
+        procedureList.add(firstProcedureNode);
+        TNode currentNode = firstProcedureNode;
+
+        while (iast.getLinkedNode(LinkType.RightSibling,currentNode) != null){
+            currentNode = iast.getLinkedNode(LinkType.RightSibling,currentNode);
+            System.out.println("currentNode = " + currentNode);
+            procedureList.add(currentNode);
+        }
+
+        for(TNode procedureNode: procedureList){
+            System.out.println("procedureNode = " + procedureNode);
+
+            generateModifies(procedureNode);
+        }
+
+
+
+
+
+        //generateModifies(tNodeRoot);
+        generateUses(tNodeRoot);
+    }
+
+    private void generateModifies(TNode tNodeProcedure){
+        TNode stmtList = iast.getFirstChild(tNodeProcedure);
+        TNode currentNode = iast.getFirstChild(stmtList);
+        TNode stmtNode = currentNode;
+        System.out.println("currentNode in modifies -> " + currentNode); //todo: jest blad z petla do while
+        do{
+            if(iast.getType(currentNode) == EntityType.ASSIGN){
+                currentNode = iast.getFirstChild(currentNode);
+                iModifies.addModifies(tNodeProcedure,stmtNode,currentNode);
+            }
+            System.out.println("currentNode in modifies -> " + currentNode);
+            currentNode = iast.getLinkedNode(LinkType.RightSibling,stmtNode);
+        }while (iast.getLinkedNode(LinkType.RightSibling,currentNode) != null);
+
+        System.out.println("End of generateModifies");
+    }
+
+    private void generateUses(TNode tNodeRoot){
+        System.out.println("generateUses - not implemented yet!");
+    }
 
 }
---
+++ src/test/java/Frontend/AbstractionExtractorTest.java
@@ -0,0 +1,112 @@
+package Frontend;
+
+import org.example.Frontend.AbstractionExtractor;
+import org.example.Frontend.IASTImplementationFrontend;
+import org.example.Frontend.ImodifiesFrontendImpl;
+import org.example.Frontend.TNodeImpl;
+import org.example.PKB.API.IAST;
+import org.example.PKB.API.IModifies;
+import org.example.PKB.API.LinkType;
+import org.example.PKB.API.TNode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class AbstractionExtractorTest {
+
+    @Mock
+    IASTImplementationFrontend iast;
+
+    @InjectMocks
+    AbstractionExtractor ae;
+
+
+    //Tutaj odbywa sie budowa drzewa AST
+    @BeforeEach
+    public void init(){
+
+        ae = new AbstractionExtractor(iast);
+
+        TNode rootNode = new TNodeImpl();
+        //procedure nazwa {
+        TNode procedureNode = new TNodeImpl();
+        TNode procedureStmtListNode = new TNodeImpl();
+        // x = 5;
+        TNode assignNode = new TNodeImpl();
+        TNode variableNode = new TNodeImpl();
+        TNode constantNode = new TNodeImpl();
+        // while x { z = 9; } }
+        TNode whileNode = new TNodeImpl();
+        TNode whileVariableNode = new TNodeImpl();
+        TNode whileStmtListNode = new TNodeImpl();
+        TNode assignNode2 = new TNodeImpl();
+        TNode variableNode2 = new TNodeImpl();
+        TNode constantNode2 = new TNodeImpl();
+
+        // New procedure block
+        TNode procedureNode2 = new TNodeImpl();
+        TNode procedureStmtListNode2 = new TNodeImpl();
+
+        TNode assignNode3 = new TNodeImpl();
+        TNode constantNode3 = new TNodeImpl();
+        TNode variableNode3 = new TNodeImpl();
+
+
+        //Budowa wszystkich relacji pomiedzy TNode'ami
+
+        when(iast.getRoot()).thenReturn(rootNode); // PROGRAM
+        when(iast.getFirstChild(rootNode)).thenReturn(procedureNode); // PROC
+        when(iast.getLinkedNode(LinkType.FirstChild,rootNode)).thenReturn(procedureNode);
+        when(iast.getLinkedNode(LinkType.RightSibling,procedureNode)).thenReturn(procedureNode2); // PROC2
+
+        when(iast.getFirstChild(procedureNode)).thenReturn(procedureStmtListNode);
+
+        when(iast.getFirstChild(procedureStmtListNode)).thenReturn(assignNode); // ASSIGN
+        when(iast.getFirstChild(assignNode)).thenReturn(variableNode); // VAR
+        when(iast.getLinkedNode(LinkType.RightSibling,variableNode)).thenReturn(constantNode); // CONST
+
+        when(iast.getLinkedNode(LinkType.RightSibling,assignNode)).thenReturn(whileNode); // WHILE
+        when(iast.getFirstChild(whileNode)).thenReturn(whileVariableNode); // VAR
+        when(iast.getLinkedNode(LinkType.RightSibling,whileVariableNode)).thenReturn(whileStmtListNode);
+        when(iast.getFirstChild(whileStmtListNode)).thenReturn(assignNode2); // ASSIGN
+        when(iast.getFirstChild(assignNode2)).thenReturn(variableNode2); // VAR
+        when(iast.getLinkedNode(LinkType.RightSibling,variableNode2)).thenReturn(constantNode2); // CONST
+        when(iast.getLinkedNode(LinkType.RightSibling,constantNode2)).thenReturn(null);
+        when(iast.getLinkedNode(LinkType.RightSibling,assignNode2)).thenReturn(null);
+        when(iast.getLinkedNode(LinkType.RightSibling,whileNode)).thenReturn(null);
+
+
+
+
+        //PROC2 kontynuacja
+        when(iast.getFirstChild(procedureNode2)).thenReturn(procedureStmtListNode2);
+        when(iast.getFirstChild(procedureStmtListNode2)).thenReturn(assignNode3); //ASSIGMENT
+        when(iast.getFirstChild(assignNode3)).thenReturn(variableNode3);  // VAR
+        when(iast.getLinkedNode(LinkType.RightSibling,variableNode3)).thenReturn(constantNode3); // CONST
+        when(iast.getLinkedNode(LinkType.RightSibling,constantNode3)).thenReturn(null);
+        when(iast.getLinkedNode(LinkType.RightSibling,assignNode3)).thenReturn(null);
+        when(iast.getLinkedNode(LinkType.RightSibling,procedureNode2)).thenReturn(null);
+
+        when(iast.getLinkedNode(any(),isNull())).thenReturn(null); // musza byc matchery
+        //when(iast.getFirstChild(null)).thenReturn(null);
+    }
+
+    @Test
+    public void modifiesTest(){
+
+
+        ae.generateStarterAbstractions();
+
+        assertTrue(true); //asercja do zmiany!
+    }
+}
