f3aeec3f21506f959248c18ed23d7c20ffb0dd9b
Squezer123 <krzysztof.kordiukiewicz@gmail.com>
2025-03-31 20:15:01 +0200

    Zrobione ProcedureNode

--- src/main/java/org/example/PKB/Source/NodeImplementations/ProcedureNode.java
+++ src/main/java/org/example/PKB/Source/NodeImplementations/ProcedureNode.java
@@ -1,4 +1,44 @@
 package org.example.PKB.Source.NodeImplementations;
 
-public class ProcedureNode {
+import org.example.PKB.Source.ASTNode;
+import org.example.Exceptions.ASTBuildException;
+import org.example.PKB.API.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProcedureNode extends ASTNode {
+    private final List<ASTNode> children;
+
+    public ProcedureNode() {
+        super(EntityType.PROCEDURE);
+        this.children = new ArrayList<ASTNode>();
+    }
+
+    @Override
+    public int setNextChild(ASTNode child) throws ASTBuildException {
+        if (child == null) {
+            throw new ASTBuildException("Can't add null child node");
+        }
+        children.add(child);
+        child.setParent(this);
+        return children.size() - 1;
+    }
+
+    @Override
+    public List<ASTNode> getChildren() {
+        return new ArrayList<>(children);  //
+    }
+
+    @Override
+    public ASTNode getChild(int num) {
+        if (num < 0 || num >= children.size()) {
+            return null;
+        }
+        return children.get(num);
+    }
+
+    @Override
+    public int getChildCount() {
+        return children.size();
+    }
 }
