eb376d9899dbcd5e4d55d5d8a7d285324e579820
LukaszPB <lukasz.kiersnowski.110752@student.pb.edu.pl>
2025-04-01 00:13:09 +0200

    added PreProcessor tests and fix some bugs

--- src/main/java/org/example/QueryProc/QueryController.java
+++ src/main/java/org/example/QueryProc/QueryController.java
@@ -10,8 +10,9 @@ public class QueryController {
         QueryTree queryTree = null;
         try {
 //            queryTree = preProc.parseQuery("stmt s1,s2; assign a; Select s1, s2 such that Modifies (s1, 2) such that Calls (s1, \"Second\") with a.procName= 19");
-            queryTree = preProc.parseQuery("stmt s1,s2,s3; procedure p; Select p, s3 such that Follows (s1,s2) such that Calls (p, \"Second\") with p.procName = \"Third\"");
-            queryTree = preProc.parseQuery("Boolean such that Follows (5,6)");
+//            queryTree = preProc.parseQuery("stmt s1,s2,s3; procedure p; Select p, s3 such that Follows (s1,s2) such that Calls (p, \"Second\") with p.procName = \"Third\"");
+//            queryTree = preProc.parseQuery("Boolean such that Follows (5,6)");
+            queryTree = preProc.parseQuery("stmt s; variable v; Select s such that Uses(s, v) with v.varName==\"x\"");
         } catch (InvalidQueryException e) {
             System.out.println(e.getMessage());
         }
