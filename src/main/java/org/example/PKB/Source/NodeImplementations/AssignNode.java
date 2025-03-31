3c2300c54b130cd322b5592ee0b8d09075c4c432
lewy <huak@wp.pl>
2025-03-31 18:55:09 +0200

    AssignNode done weryfka

--- src/main/java/org/example/PKB/Source/NodeImplementations/AssignNode.java
+++ src/main/java/org/example/PKB/Source/NodeImplementations/AssignNode.java
@@ -4,10 +4,13 @@ import org.example.Exceptions.ASTBuildException;
 import org.example.PKB.API.EntityType;
 import org.example.PKB.Source.ASTNode;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
 public class AssignNode  extends StmtNode{
+    private VariableNode variableNode;
+    private ExprNode exprNode;
     public AssignNode() {
         super(EntityType.ASSIGN);
     }
@@ -21,21 +24,57 @@ public class AssignNode  extends StmtNode{
 
     @Override
     public int setNextChild(ASTNode child) throws ASTBuildException {
-        return 0;
+        if(variableNode == null)
+        {
+            if(!(child instanceof VariableNode))
+                throw new ASTBuildException("Variable child must be an instance of VariableNode");
+            variableNode = (VariableNode) child;
+            variableNode.setId(0);
+            variableNode.setParent(this);
+            return 0;
+        }
+        if(exprNode == null)
+        {
+            if(!(child instanceof ExprNode))
+            {
+                throw new ASTBuildException("ExprNode child must be an instance of ExprNode");
+            }
+            exprNode = (ExprNode) child;
+            exprNode.setId(1);
+            exprNode.setParent(this);
+            return 1;
+        }
+        return -1;
     }
 
     @Override
     public List<ASTNode> getChildren() {
-        return List.of();
+        if (variableNode == null) {
+            return List.of();
+        }
+        List<ASTNode> children = new ArrayList<>();
+        children.add(variableNode);
+        children.add(exprNode);
+        return children;
     }
 
     @Override
     public ASTNode getChild(int num) {
+        if(num == 0)
+            return variableNode;
+        if(num == 1)
+            return exprNode;
         return null;
     }
 
     @Override
     public int getChildCount() {
+        if(variableNode != null)
+        {
+            if(exprNode != null)
+                return 2;
+            return 1;
+        }
         return 0;
     }
 }
