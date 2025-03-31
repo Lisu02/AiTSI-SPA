d096900eb48b17dc0df4e533a9da2b85c847c637
casii-21 <k_sawicki@hotmail.com>
2025-03-31 16:48:37 +0200

    EvaluatorTest do przedyskutowania na zajeciach

--- src/main/java/org/example/QueryProc/QueryController.java
+++ src/main/java/org/example/QueryProc/QueryController.java
@@ -11,7 +11,7 @@ public class QueryController {
         try {
 //            queryTree = preProc.parseQuery("stmt s1,s2; assign a; Select s1, s2 such that Modifies (s1, 2) such that Calls (s1, \"Second\") with a.procName= 19");
             queryTree = preProc.parseQuery("stmt s1,s2,s3; procedure p; Select p, s3 such that Follows (s1,s2) such that Calls (p, \"Second\") with p.procName = \"Third\"");
-            queryTree = preProc.parseQuery("Boolean such that Follows 5,6)");
+            queryTree = preProc.parseQuery("Boolean such that Follows (5,6)");
         } catch (InvalidQueryException e) {
             System.out.println(e.getMessage());
         }
