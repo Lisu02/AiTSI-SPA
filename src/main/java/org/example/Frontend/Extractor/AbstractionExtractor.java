85be8f1e7b5c5901a9db7a5ee775440053fc36e7
Lisu02 <terminatorektt8000@gmail.com>
2025-05-03 17:34:31 +0200

    AbstractionExtractor refactor (SOLID principles)

---
+++ src/main/java/org/example/Frontend/Extractor/AbstractionExtractor.java
@@ -0,0 +1,67 @@
+    package org.example.Frontend.Extractor;
+
+    import org.example.PKB.API.*;
+    import org.example.PKB.Source.ASTImplementations.ASTModifies;
+    import org.example.PKB.Source.ASTImplementations.ASTUses;
+
+    import java.util.ArrayList;
+    import java.util.List;
+    import java.util.Stack;
+    import java.util.logging.Logger;
+
+    public class AbstractionExtractor {
+
+        private static final Logger log = Logger.getLogger(AbstractionExtractor.class.getName());
+
+        //Na razie testowo imodifies
+        private IAST iast;
+        private IModifies iModifies;
+        private IUses iUses;
+        private ICalls iCalls;
+        private ModifiesExtractor modifiesExtractor;
+        private UsesExtractor usesExtractor;
+        private CallsExtractor callsExtractor;
+
+        public AbstractionExtractor(){
+            this(PKB.getAST(),ASTModifies.getInstance(),ASTUses.getInstance(),null);
+        }
+        public AbstractionExtractor(IAST iast){
+            this(iast,ASTModifies.getInstance(),ASTUses.getInstance(),null);
+        }
+        public AbstractionExtractor(IAST iast, IModifies iModifies,IUses iuses, ICalls iCalls){
+            this.iast = iast;
+            this.iModifies = iModifies;
+            this.iUses=iuses;
+            this.modifiesExtractor = new ModifiesExtractor(iast,iModifies);
+            this.usesExtractor = new UsesExtractor(iast,iUses);
+            this.callsExtractor = new CallsExtractor(iast,iCalls);
+        }
+
+    //Generowanie Modifies oraz Uses do PKB
+    public void generateStarterAbstractions(){
+        TNode tNodeRoot = iast.getRoot();
+        List<TNode> procedureList = new ArrayList<>();
+        TNode firstProcedureNode = iast.getLinkedNode(LinkType.FirstChild,tNodeRoot);
+        procedureList.add(firstProcedureNode);
+
+        TNode currentNode = firstProcedureNode;
+        while (iast.getLinkedNode(LinkType.RightSibling,currentNode) != null){
+            currentNode = iast.getLinkedNode(LinkType.RightSibling,currentNode);
+            log.fine("currentNode = " + currentNode);
+            procedureList.add(currentNode);
+        }
+        log.info("Amount of detected procedures -> {" + procedureList + "}\n");
+
+        for(TNode procedureNode: procedureList){
+            modifiesExtractor.extract(procedureNode,iast.getFirstChild(procedureNode));
+            usesExtractor.extract(procedureNode,iast.getFirstChild(procedureNode),null);
+        }
+    }
+
+
+
+
+
+
+
+}
