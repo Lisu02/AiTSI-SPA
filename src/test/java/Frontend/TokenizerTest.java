4c74c59081a4e4a48f918b0e4147ddf2c60f11d0
Lisu02 <terminatorektt8000@gmail.com>
2025-05-04 20:29:30 +0200

    Bug Fix Tokenizer + new tests

--- src/test/java/Frontend/TokenizerTest.java
+++ src/test/java/Frontend/TokenizerTest.java
@@ -2,10 +2,16 @@ package Frontend;
 
 import org.example.Frontend.Tokenizer;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -18,6 +24,76 @@ public class TokenizerTest {
     @BeforeEach
     void init(){
         tokenizer = Tokenizer.getInstance();
+    }
+
+    @Test
+    @DisplayName("Slicing: +pepperonii+ should return 3 tokens")
+    public void shouldSliceWordWithPlusSignsAroundWord(){
+        //given
+        String word = "+pepperonii+";
+        //when
+        List<String> slicedWord = tokenizer.sliceWord(word);
+        //then
+        assertIterableEquals(Arrays.asList("+","pepperonii","+"),slicedWord);
+    }
+
+    @Test
+    @DisplayName("Slicing: +1+ should return 3 tokens")
+    public void shouldSliceWordWithPlusSignsAroundSingleNumber(){
+        //given
+        String word = "+1+";
+        //when
+        List<String> slicedWord = tokenizer.sliceWord(word);
+        //then
+        assertIterableEquals(Arrays.asList("+","1","+"),slicedWord);
+    }
+
+    @Test
+    @DisplayName("Slicing: +123+ should return 3 tokens")
+    public void shouldSliceWordWithPlusSignsAroundThreeNumbers(){
+        //given
+        String word = "+123+";
+        //when
+        List<String> slicedWord = tokenizer.sliceWord(word);
+        //then
+        assertIterableEquals(Arrays.asList("+","123","+"),slicedWord);
+    }
+
+    @Test
+    public void checkProperTokenizationProcessWithWhileAndCallsAndIf(){
+        //given
+        URL resource = getClass().getClassLoader()
+                .getResource("frontend/tokenizerTestFile.txt");
+        if (resource == null){fail("No file found");}
+        File file = new File(resource.getFile());
+        String absolutePath = file.getAbsolutePath();
+
+        //when
+        List<String> tokenList = tokenizer.getTokensFromFilename(absolutePath);
+
+        //then
+        List<String> expectedList = new LinkedList<String>(Arrays.asList(
+                "procedure","First","{"
+                ,"x","=","5",";",
+                "a","=","x","+","3","+","1",";",
+                "c","=","x","+","a","+","1","+","2","+","5",";",
+                "while","c","{",
+                "c","=","c","-","1",";",
+                "}",
+                "if","a","then","{",
+                "call","Second",";",
+                "}","else","{",
+                "a","=","0",";",
+                "}",
+                "x","=","0",";",
+                "}",
+                "procedure","Second","{",
+                "p","=","10",";",
+                "d","=","20",";",
+                "l","=","d","+","p",";",
+                "}"
+        ));
+        assertIterableEquals(expectedList,tokenList);
     }
 
     @Test
